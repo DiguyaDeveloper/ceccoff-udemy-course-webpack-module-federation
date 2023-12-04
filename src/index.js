906f8c3154987bfb94b74c046cb58d30bb307e8d
DiguyaDeveloper <diegocecconcwb@outlook.com>
2023-12-04 20:04:22 -0300

    feat: ✨ Consumindo JSON sem loader padrão v2+ webpack

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import Button from './components/button/button';
 import Warning from './templates/warning.html';
 import './templates/warning.css';
 import PhraseTxt from './files/file.txt';
+import DescriptionJson from './files/description.json';
 
 const title = new Title();
 
@@ -20,4 +21,5 @@ button.create();
 const body = document.querySelector('body');
 body.innerHTML += PhraseTxt;
 body.innerHTML += Warning;
+body.innerHTML += DescriptionJson;
 
