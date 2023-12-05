028a52b70ded8cc5c26e27fd5c2f767d8a152e32
DiguyaDeveloper <diegocecconcwb@outlook.com>
2023-12-04 21:50:28 -0300

    feat: ✨ Adicionando icones svg do fontawesome

--- aula-sobre-integracoes/src/index.js
+++ aula-sobre-integracoes/src/index.js
@@ -2,8 +2,19 @@
 
 import './fonts.css';
 import $ from 'jquery';
+import { library, dom } from '@fortawesome/fontawesome-svg-core';
+import { faBacon } from '@fortawesome/free-solid-svg-icons';
+
+library.add(faBacon);
+dom.watch();
 
 const body = $('body');
+
+const h1 = $("<h1></h1>").text("Meu titulo com icone").css("color", "blue");
+const icon = $("<i class='fas fa-bacon'></i>");
+
+body.append(h1);
+body.append(icon);
 
 const p = $("<p></p>").text("Texto no paragrafo").css("color", "red");
 
