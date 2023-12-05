059b4fa6da8e5ed80960ec979146cc82428688c6
DiguyaDeveloper <diegocecconcwb@outlook.com>
2023-12-05 20:42:30 -0300

    feat: ✨ Adicionado aula sobre HMR

---
+++ aula-sobre-melhora-HMR/webpack.config.js
@@ -0,0 +1,31 @@
+const path = require("path");
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: "./src/index.js",
+    output: {
+        path: path.resolve(__dirname, "./dist"),
+        filename: "[name].js"
+    },
+    devServer: {
+        historyApiFallback: true
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                includes: path.resolve(__dirname, 'src')
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Recursos Webpack'
+        })
+    ],
+    optimization: {
+        runtimeChunk: true
+    }
+};
