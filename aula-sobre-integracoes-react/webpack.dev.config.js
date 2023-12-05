0a84bd0fa47c7e6942e79da4b4f96365a1a15a2e
DiguyaDeveloper <diegocecconcwb@outlook.com>
2023-12-04 22:08:40 -0300

    feat: ✨ Criando projeto base para aula de integracoes com react

---
+++ aula-sobre-integracoes-react/webpack.dev.config.js
@@ -0,0 +1,37 @@
+const path = require("path");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+    entry: "./src/index.js",
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "bundle.js",
+    },
+    mode: 'development',
+    devServer: {
+        port: 8000,
+        historyApiFallback: {
+            index: 'index.html'
+        }
+    },
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            title: 'Integrações Bulma'
+        }),
+        new CleanWebpackPlugin(),
+        new MiniCssExtractPlugin({
+            filename: '[name].[contenthash].css'
+        }),
+    ]
+};
