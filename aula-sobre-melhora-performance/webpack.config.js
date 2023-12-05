f6ca22905883bbb2ec7659966bda5aa0c6d3ad7e
DiguyaDeveloper <diegocecconcwb@outlook.com>
2023-12-05 20:31:38 -0300

    feat: :sparkle: Criando estrutura projeto

---
+++ aula-sobre-melhora-performance/webpack.config.js
@@ -0,0 +1,15 @@
+const path = require("path");
+const webpack = require('webpack');
+
+module.exports = {
+    entry: "./src/index.js",
+    output: {
+        path: path.resolve(__dirname, "./dist"),
+        filename: "bundle.js"
+    },
+    plugins: [
+        new webpack.ProvidePlugin({
+            _: 'lodash'
+        })
+    ],
+};
