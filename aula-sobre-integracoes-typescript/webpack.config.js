57b36fafd51e5db8c0f89ff1aaaa7bd8a25d3559
DiguyaDeveloper <diegocecconcwb@outlook.com>
2023-12-04 23:10:12 -0300

    feat: ✨ Criando exemplo de integracao com typescript

---
+++ aula-sobre-integracoes-typescript/webpack.config.js
@@ -0,0 +1,25 @@
+const path = require("path");
+
+module.exports = {
+    entry: "./src/index.ts",
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "bundle.js"
+    },
+    mode: 'development',
+    devServer: {
+        historyApiFallback: true
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js']
+    }
+};
