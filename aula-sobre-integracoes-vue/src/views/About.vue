6c3d1b29849696e54bc68c9add34efbdcb23c289
DiguyaDeveloper <diegocecconcwb@outlook.com>
2023-12-04 23:01:40 -0300

    feat: ✨ Criando components com vue para exemplo

---
+++ aula-sobre-integracoes-vue/src/views/About.vue
@@ -0,0 +1,5 @@
+<template>
+  <div id="about">
+    <h1>About page here</h1>
+  </div>
+</template>
---
+++ aula-sobre-integracoes-vue/src/views/Home.vue
@@ -0,0 +1,5 @@
+<template>
+  <div id="home">
+    <h1>Home Hello World!</h1>
+  </div>
+</template>
