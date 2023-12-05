1ca7e14f65496ff1850d75ad991faaccd90f7054
DiguyaDeveloper <diegocecconcwb@outlook.com>
2023-12-04 21:17:07 -0300

    feat: ✨ Adicionando exemplo de integracao com jquery

---
+++ aula-sobre-integracoes/src/index.js
@@ -0,0 +1,7 @@
+import $ from 'jquery';
+
+const body = $('body');
+
+const p = $("<p></p>").text("Texto no paragrafo").css("color", "red");
+
+body.append(p);
---
+++ aula-sobre-integracoes/webpack.production.config.js
@@ -0,0 +1,33 @@
+const path = require("path");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const TerserPlugin = require("terser-webpack-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+    entry: "./src/index.js",
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "[name].[contenthash].js",
+    },
+    mode: 'production',
+    optimization: {
+        minimize: true,
+        minimizer: [new TerserPlugin()]
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [MiniCssExtractPlugin.loader, "css-loader"],
+            },
+        ],
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new MiniCssExtractPlugin({
+            filename: '[name].[contenthash].css'
+        }),
+        new HtmlWebpackPlugin(),
+    ]
+};
