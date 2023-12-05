4a63c7fe5e025415f37d8e012b2094c42cc4df38
DiguyaDeveloper <diegocecconcwb@outlook.com>
2023-12-05 20:30:48 -0300

    fix: :sneak: Removido typescript e ajustado ProviderPlugin

--- aula-sobre-shiming/webpack.config.js
+++ aula-sobre-shiming/webpack.config.js
@@ -2,29 +2,13 @@ const path = require("path");
 const webpack = require('webpack');
 
 module.exports = {
-    entry: "./src/index.ts",
+    entry: "./src/index.js",
     output: {
-        path: path.resolve(__dirname, "dist"),
+        path: path.resolve(__dirname, "./dist"),
         filename: "bundle.js"
     },
-    mode: 'development',
-    devServer: {
-        historyApiFallback: true
-    },
-    module: {
-        rules: [
-            {
-                test: /\.tsx?$/,
-                use: 'ts-loader',
-                exclude: /node_modules/
-            },
-        ],
-    },
-    resolve: {
-        extensions: ['.tsx', '.ts', '.js']
-    },
     plugins: [
-        new webpack.ProviderPlugin({
+        new webpack.ProvidePlugin({
             _: 'lodash'
         })
     ],
